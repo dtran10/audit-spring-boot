76b5cc066609e0e319162e38008e1db537a89a2e
Tran <david.b.tran@accenturefederal.com>
2017-04-07 09:42:29 -0400

    reformating code to be readable for swagger

--- src/main/java/com/dtran10/service/DocumentationConfig.java
+++ src/main/java/com/dtran10/service/DocumentationConfig.java
@@ -60,8 +60,11 @@ public class DocumentationConfig {
 	 */
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build().apiInfo(apiInfo());
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
 	}
 
 	private ApiInfo apiInfo() {
